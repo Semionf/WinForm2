0ae8675c0559026b6f89d2ab07408be4b949f1f8
semionf <sfurlender@gmail.com>
2022-11-28 15:16:45 +0200

    .

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += button1_Click;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                label3.Text += (Convert.ToInt32(textBox1.Text) + Convert.ToInt32(textBox2.Text));
+
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+            
+        }
+
+        
+    }
+}
